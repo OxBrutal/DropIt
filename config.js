c86ac25403bbce8462d95b70f31e0c55f102ced0
QyuuNee <61906304+OxBrutal@users.noreply.github.com>
2023-09-19 19:56:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    dbURL: process.env.DATABASEURL || "mongodb://localhost/dropit",
+    dbURL: process.env.DATABASEURL || "mongodb+srv://RBWorld:3d6ubjpQrt6cNFmn@uptimer.5t4em.mongodb.net/?retryWrites=true&w=majority",
     port: process.env.PORT || 7000
 }
